1707d525ba73b16bf2b8ccede47c4aff570427cc
cronopio <daristizabal@kumokojo.com>
2011-06-12 23:42:42 -0500

    [general] cambio orden de config de express

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(express.cookieParser());
+  app.use(express.methodOverride());  
   app.use(express.session({secret:'bitcoinpriceonfacebook'}));
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
 app.configure('development', function(){
